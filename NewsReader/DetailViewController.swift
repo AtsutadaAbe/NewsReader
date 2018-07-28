8426fd47167a399168f9853ce9a20b1711a19afd
AtsutadaAbe <a.a.bene19751206u@gmail.com>
2018-07-28 23:00:18 +0900

    ニュースリーダーアプリ

---
+++ NewsReader/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  NewsReader
+//
+//  Created by 阿部敦忠 on 2018/07/28.
+//  Copyright © 2018年 阿部敦忠. All rights reserved.
+//
+
+import UKkit
+import WebKit
+
+class DetailViewController : UIViewController{
+    
+}
---
+++ NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 阿部敦忠 on 2018/07/28.
+//  Copyright © 2018年 阿部敦忠. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
